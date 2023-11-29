08d04c4c23fbd5de1ce4268f2723c8c3a0e73a17
chisalabharzi <chisalabharzi17@gmail.com>
2023-11-29 10:24:13 -0800

    2-execute_a_command.pp

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,8 @@
-# Kills killmenow process
+# manifest that kills a process named killmenow, with puppet
 
 exec { 'pkill killmenow':
-  path    => '/usr/bin/',
-  command => 'pkill -x killmenow'
+  path     => '/usr/bin',
+  command  => 'pkill killmenow',
+  provider => shell,
+  returns  => [0, 1]
 }
