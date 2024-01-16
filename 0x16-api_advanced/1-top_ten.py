5806cf2ca46b5c7de00f0a1e81cca02a30960a1a
chisalabharzi <chisalabharzi17@gmail.com>
2024-01-16 12:20:24 -0800

    api_adva

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Function that queries Reddit API and prints first 10 titles."""
+
+import requests
+
+
+def top_ten(subreddit):
+    """
+    function that Print the titles of the 10 hottest posts
+    """
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    params = {
+        "limit": 10
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
+        print("None")
+        return
+    results = response.json().get("data")
+    [print(c.get("data").get("title")) for c in results.get("children")]
